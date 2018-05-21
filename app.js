0448a8c98ef8c568677ad06dbc3509494a789f5c
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-05-21 14:13:15 -0500

    no mongoose yet

--- app.js
+++ app.js
@@ -32,68 +32,15 @@ app.post('/', function(req, res) {
 	res.json('challenge key')
  });
 
-// EXAMPLES
-
-// FOR A TEAM
-// {
-//     "_id": {
-//         "$oid": "5ae78652734d1d133184096f"
-//     },
-//     "name": "Australia",
-//     "flag": "au",
-//     "points": 0,
-//     "tournament": "women",
-//     "abb": "AUS"
-// }
-
-
-// FOR A GAME
-// {
-//     "_id": {
-//         "$oid": "5ae8aeb9734d1d1331848a12"
-//     },
-//     "team1": "Tm 1",
-//     "score1": 0,
-//     "team2": "Tm 2",
-//     "score2": 0,
-//     "type": "knockout",
-//     "tournament": "co-rec",
-//     "game": 19
-// }
-
-
-// KNOCKOUT GAME
-
-// Find that game and update the teams
-
-
-
-// Body sent like this from Slack:
-
-// token=gIkuvaNzQIHg97ATvDxqgjtO
-// team_id=T0001
-// team_domain=example
-// enterprise_id=E0001
-// enterprise_name=Globular%20Construct%20Inc
-// channel_id=C2147483705
-// channel_name=test
-// user_id=U2147483697
-// user_name=Steve
-// command=/addscore
-// text=Mexico 2 Mozambique 2 co-rec group
-// response_url=https://hooks.slack.com/commands/1234/5678
-// trigger_id=13345224609.738474920.8088930838d88f008e0
-
-
 
 //====MONGOOSE CONNECT===//
-mongoose.connect(url, function (err, db) {
-	if (err) {
-		console.log('Unable to connect to the mongoDB server. Error:', err);
-	} else {
-		console.log('Connection established to', url);
-	}
-});
+// mongoose.connect(url, function (err, db) {
+// 	if (err) {
+// 		console.log('Unable to connect to the mongoDB server. Error:', err);
+// 	} else {
+// 		console.log('Connection established to', url);
+// 	}
+// });
 
 app.listen(process.env.PORT || 4000);
 console.log('starting the application');
