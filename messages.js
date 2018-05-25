44215708bc2d9db8a47b02bfb3b652ac800557c3
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-05-25 09:59:57 -0500

    string to json

--- messages.js
+++ messages.js
@@ -1,31 +1,20 @@
-export const mentor_or_mentee_message = { "text": "Hi! Before you start with Kyte we need to ask one quick question : )",
-                                            "attachments": [
-                                                {
-                                                    "text": "How old are you?",
-                                                    "fallback": "You are unable to choose a game",
-                                                    "callback_id": "mentor_or_mentee",
-                                                    "color": "#3AA3E3",
-                                                    "attachment_type": "default",
-                                                    "actions": [
-                                                        {
-                                                            "name": "mentee",
-                                                            "text": "High School",
-                                                            "type": "button",
-                                                            "value": "highschool"
-                                                        },
-                                                        {
-                                                            "name": "mentor",
-                                                            "text": "College",
-                                                            "type": "button",
-                                                            "value": "college"
-                                                        },
-                                                        {
-                                                            "name": "mentor",
-                                                            "text": "Industry Professional",
-                                                            "type": "button",
-                                                            "value": "industry"
-                                                        }
-                                                    ]
-                                                }
-                                            ]
-                                        };
+export const mentor_or_mentee_message = JSON.parse('{ "text": "Hi! Before you start with Kyte we need to ask one quick question : )",' +
+                                            '"attachments": [' +
+                                                '{  "text": "How old are you?",' +
+                                                    '"fallback": "You are unable to choose a game",' +
+                                                    '"callback_id": "mentor_or_mentee",' +
+                                                    '"color": "#3AA3E3",' +
+                                                    '"attachment_type": "default",' +
+                                                    '"actions": [' +
+                                                        '{  "name": "mentee",' +
+                                                            '"text": "High School",' +
+                                                            '"type": "button",' +
+                                                            '"value": "highschool" },' +
+                                                        '{  "name": "mentor",' +
+                                                            '"text": "College",' +
+                                                            '"type": "button",' +
+                                                            '"value": "college" },' +
+                                                        '{  "name": "mentor",' +
+                                                            '"text": "Industry Professional",' +
+                                                            '"type": "button",' +
+                                                            '"value": "industry" }]}]}');
