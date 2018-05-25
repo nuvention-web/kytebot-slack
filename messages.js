dc275a2d7ee52fdbe3e21f49e4d6ef3a813c460d
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-05-25 09:49:47 -0500

    changing export

--- messages.js
+++ messages.js
@@ -1,34 +1,31 @@
-export function mentor_or_mentee() {
-     var mentor_or_mentee_message = { "text": "Hi! Before you start with Kyte we need to ask one quick question : )",
-                                        "attachments": [
-                                            {
-                                                "text": "How old are you?",
-                                                "fallback": "You are unable to choose a game",
-                                                "callback_id": "mentor_or_mentee",
-                                                "color": "#3AA3E3",
-                                                "attachment_type": "default",
-                                                "actions": [
-                                                    {
-                                                        "name": "mentee",
-                                                        "text": "High School",
-                                                        "type": "button",
-                                                        "value": "highschool"
-                                                    },
-                                                    {
-                                                        "name": "mentor",
-                                                        "text": "College",
-                                                        "type": "button",
-                                                        "value": "college"
-                                                    },
-                                                    {
-                                                        "name": "mentor",
-                                                        "text": "Industry Professional",
-                                                        "type": "button",
-                                                        "value": "industry"
-                                                    }
-                                                ]
-                                            }
-                                        ]
-                                    };
-    return mentor_or_mentee_message;
-}
+export const mentor_or_mentee_message = { "text": "Hi! Before you start with Kyte we need to ask one quick question : )",
+                                            "attachments": [
+                                                {
+                                                    "text": "How old are you?",
+                                                    "fallback": "You are unable to choose a game",
+                                                    "callback_id": "mentor_or_mentee",
+                                                    "color": "#3AA3E3",
+                                                    "attachment_type": "default",
+                                                    "actions": [
+                                                        {
+                                                            "name": "mentee",
+                                                            "text": "High School",
+                                                            "type": "button",
+                                                            "value": "highschool"
+                                                        },
+                                                        {
+                                                            "name": "mentor",
+                                                            "text": "College",
+                                                            "type": "button",
+                                                            "value": "college"
+                                                        },
+                                                        {
+                                                            "name": "mentor",
+                                                            "text": "Industry Professional",
+                                                            "type": "button",
+                                                            "value": "industry"
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        };
