32e884d32e6b249d4abe4111ad792bb36eed383d
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-06-04 22:54:38 -0500

    tracking time

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ const url = 'mongodb://admin:kyteadmin@ds231070.mlab.com:31070/kyte-slack';
 const axios = require('axios');
 // const messages = require('./messages');
 
+// SLACK TESTS TOKEN FOR SENDING DMS
+// xoxp-210179574949-210004780787-375259193188-8296155003a4cb704a1906f513f6ad0b
+// curl --header "Content-Type: application/json" --request POST --data '{"ok": true, "channel": "U6604NYP5", "message": {"text": "Test message for dm", "username": "Kytebot", "type": "message", "subtype": "bot_message", "bot_id": "AAUJH6RK9"}}' https://slack.com/api/chat.postMessage?token=xoxp-210179574949-210004780787-375259193188-8296155003a4cb704a1906f513f6ad0b&channel=U6604NYP5&text=A%20direct%20message&pretty=1
+// {
+//     "ok": true,
+//     "channel": "D66SUUBSB",
+//     "ts": "1528143385.000209",
+//     "message": {
+//         "text": "A direct message",
+//         "username": "Slack API Tester",
+//         "bot_id": "BB1JA71SR",
+//         "type": "message",
+//         "subtype": "bot_message",
+//         "ts": "1528143385.000209"
+//     }
+// }
+
+// Different authentication for bot
+// xoxb-210179574949-367778535666-Tk3dBc3tjvW9amgQG5gc9UyE
+
 // For cors
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -57,13 +77,68 @@ app.post('/activity', function(req, res) {
 		default:
 			console.log('The action is not something we are storing in the database')
 	}
-	//res.json({'challenge': req.body.challenge});
-	res.json(req.body)
+	res.json({'challenge': req.body.challenge});
+	// res.json(req.body)
  });
 
 app.post('/options-load-endpoint', function(req, res) { 
 
 });
+
+let checkActivity = setInterval(checkTimes, 1000 * 10);
+// 1000 * 60 * 60 * 24 checks once a day * 2 would be every two days
+// let checkActivity = setInterval(checkTimes, 1000 * 60 * 60 * 12);
+// Checking every 12 hours
+
+function checkTimes() {
+	var currentTime = (new Date).getTime();
+	Triad.find({}, (err, triads) => {
+		if (err) {
+			console.log("Error in finding all triads");
+		}
+		if (triads) {
+			for (var i = 0; i < triads.length; i++) {
+				if (currentTime - triads[i].lastMessageMentor1 > 1000 ) {
+					// 1000 * 60 * 60 * 12 * 24 * 2
+					// Send message to the mentor1 to be more active in their triad
+					console.log(triads[i].mentor1 + ' was reminded to send a message to their triad at ' + currentTime);
+				}
+				if (currentTime - triads[i].lastMessageMentor2 > 1000 ) {
+					// 1000 * 60 * 60 * 12 * 24 * 2
+					// Send message to the mentor1 to be more active in their triad
+					console.log(triads[i].mentor2 + ' was reminded to send a message to their triad at ' + currentTime);
+				}
+				if (currentTime - triads[i].lastMessageMentee > 1000 ) {
+					// 1000 * 60 * 60 * 12 * 24 * 2
+					// Send message to the mentor1 to be more active in their triad
+					console.log(triads[i].mentee + ' was reminded to send a message to their triad at ' + currentTime);
+				}
+				
+				// Check each member time and compare it to the current epoch time
+
+			}
+			// User.find({}, (err, users) => {
+			// 	if (err) {
+			// 		console.log("Error finding all the users");
+			// 	}
+			// 	if (users) {
+			// 		for (var i = 0; i < users.length; i++) {
+			// 			console.log(users[i]);
+			// 			// Check each member time and compare it to the current epoch time
+
+			// 		}
+			// 	}
+			// });
+		}
+	});
+
+	// MODIFY MESSAGES TO RECORD TIMES
+	// check the seconds on every user within a triad
+	// Check triad first
+	// If have not been active for certain period of time, then send them a personal message from the application reminding them to be more active in their triad. 
+	// If they have not been active in triad, add them to array
+	// If have been 
+}
 
 function newChannel(body) {
 	var new_triad = new Triad({ triadName: body.event.channel.name,
@@ -73,7 +148,10 @@ function newChannel(body) {
 								mentee: '',
 								mentor1Messages: 0,
 								mentor2Messages: 0,
-								menteeMessages: 0});
+								menteeMessages: 0,
+								lastMessageMentor1: 0,
+								lastMessageMentor2: 0,
+								lastMessageMentee: 0});
 
 	User.findOne({'user': body.event.channel.creator}, (err, user) => {
 		if (err) {
@@ -97,8 +175,6 @@ function newChannel(body) {
 			}
 		});
 	});
-
-	
 }
 
 function joinChannel(body) {
@@ -159,13 +235,16 @@ function triadMessage(body) {
 			// Look at user id and add to the correct mentor/mentee
 			if (body.event.user === triad.mentor1) {
 				triad.mentor1Messages = triad.mentor1Messages + 1;
+				triad.lastMessageMentor1 = body.event_time;
 			}
 			else if (body.event.user === triad.mentor2) {
 				triad.mentor2Messages = triad.mentor2Messages + 1;
+				triad.lastMessageMentor2 = body.event_time;
 			}
 			else {
 				// It was the mentee
 				triad.menteeMessages = triad.menteeMessages + 1;
+				triad.lastMessageMentee = body.event_time;
 			}
 			triad.save((err) => {
 				if (err) {
@@ -184,6 +263,7 @@ function handleMessage(body) {
 		if (user) {
 			user.totalMessages = user.totalMessages + 1;
 			user.messages = user.messages + 1;
+			user.lastMessage = body.event_time;
 			user.save((err) => {
 				if (err) {
 			  		console.log("Error adding messagge info to mlab", err);
