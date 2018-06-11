1f2bb01bcb43150979d42b60f552f2115c8149b6
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-06-11 14:11:43 -0500

    responding to url

--- app.js
+++ app.js
@@ -278,12 +278,15 @@ app.post('/quickreport', function(req, res) {
 
 	var text = req.body;
 	var channel = text.channel_id;
+	var responseURL = text.response_url;
 	console.log(text.text);
 
 	if (text.text === "\"Fabian Gomez\"" || text.text === "“Fabian Gomez”") {
 		var message = "*Fabian Gomez - Mentor*\nTotal Messages: 73\nMessages This Week: 31\nLinks Sent:\n\thttps://undergradaid.northwestern.edu/docs/FinancialAidBrochure2017-18.pdf\n\thttps://www.questbridge.org/high-school-students/national-college-match/how-to-apply";
-		message.replace(/ /g,"%20");
-		axios.post('https://slack.com/api/chat.postMessage?token=' + bot_token + '&channel=' + channel + '&text=' + message + '&pretty=1');
+		axios.post(responseURL, {"response_type": "in_channel",
+	    "text": message});
+		// message.replace(/ /g,"%20");
+		// axios.post('https://slack.com/api/chat.postMessage?token=' + bot_token + '&channel=' + channel + '&text=' + message + '&pretty=1');
 	}
 	else {
 		var message = "*Johnny Garcia - Mentee*\nTotal Messages: 54\nMessages This Week: 18";
